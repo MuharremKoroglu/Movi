011fd69b4f55b562816fe69fc9ae461975e2a9e4
Muharrem Koroglu <muharrem-koroglu.99@hotmail.com>
2023-02-13 11:10:37 +0300

    Initial Commit

---
+++ Movi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movi
+//
+//  Created by Muharrem Köroğlu on 13.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
